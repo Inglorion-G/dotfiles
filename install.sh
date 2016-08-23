57133f6dce6542fe25bc6ef6ec6c0d8dd71fc586
George Groh <george.groh@gmail.com>
2016-08-23 09:22:27 -0700

    Update install.sh - timestamp archived files, homebrew permissions, vim temp

--- install.sh
+++ install.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
 echo ""
-echo "Archiving any existing configuration files..."
 
 ARCHIVE_PATH=$HOME"/dotfiles/archive/"
 DOTFILES_PATH=$HOME"/dotfiles/"
-#DOT_FILES=(".bash_profile" ".bashrc" ".tmux.conf" ".vimrc")
-DOT_FILES=(".test_me" ".test_me_too")
+DOT_FILES=(".bash_profile" ".bashrc" ".tmux.conf" ".vimrc")
+timestamp=$(date +%s)
 
-# create archive folder
+echo "Archiving any existing configuration files..."
+echo "Creating symbolic links to dotfiles..."
+
+# create an archive directory $HOME/dotfiles/archive
 mkdir -p ${ARCHIVE_PATH}
 
+# back up and symlink .files from $HOME -> $HOME/dotfiles
 for FILENAME in "${DOT_FILES[@]}"
 do
   FILEPATH="$HOME/$FILENAME"
 
   if [ -f "$FILEPATH" ]; then
-    mv "$FILEPATH" "$ARCHIVE_PATH"
-  fi 
-done
+    mv "$FILEPATH" "$ARCHIVE_PATH/$FILENAME$timestamp"
+  fi
 
-echo "Creating symbolic links to dotfiles..."
-
-for FILENAME in "${DOT_FILES[@]}"
-do
-  FILEPATH="$HOME/$FILENAME"
   SYMLINKPATH="$DOTFILES_PATH$FILENAME"
   ln -s "$SYMLINKPATH" "$FILEPATH"
 done
+
+echo "Checking permissions for Homebrew..."
+
+# check for homebrew path and correct permissions
+HOMEBREW_PREFIX="/usr/local"
+
+if [ -d "$HOMEBREW_PREFIX" ]; then
+  if ! [ -r "$HOMEBREW_PREFIX" ]; then
+    sudo chown -R "$LOGNAME:admin" /usr/local
+  fi
+else
+  sudo mkdir "$HOMEBREW_PREFIX"
+  sudo chflags norestricted "$HOMEBREW_PREFIX"
+  sudo chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
+fi
+
+echo "Making vim backup/swap directory..."
+
+# backup dir for vim
+if ! [ -d "$HOME/.vim_temp/" ]; then
+  mkdir $HOME/.vim_temp
+fi
 
 echo "Done!"
